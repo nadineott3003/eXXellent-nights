442b9b7ffd2ecf5985698b0b3f520752d7aad98a
Nadine Ott <info@nadineott.de>
2025-12-08 23:52:27 +0100

    [frontend] openapi + code formatting setup

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,11 +5,15 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideApiConfiguration } from './core/api/generated/api-configuration';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideHttpClient(),
+    provideApiConfiguration('http://localhost:8080'),
   ],
 };
