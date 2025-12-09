529b22d064108122f416df4e0629372bc98fe6a4
Nadine Ott <info@nadineott.de>
2025-12-09 17:11:10 +0100

    [frontend] add availability page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { MainLayoutComponent } from './layout/main-layout/main-layout.component'
 import { RoomListComponent } from './features/rooms/pages/room-list/room-list.component';
 import { RoomCreateComponent } from './features/rooms/pages/room-create/room-create.component';
 import { RoomEditComponent } from './features/rooms/pages/room-edit/room-edit.component';
+import { AvailabilityComponent } from './features/availability/pages/availability.component';
 
 export const appRoutes: Route[] = [
   {
@@ -20,6 +21,10 @@ export const appRoutes: Route[] = [
       {
         path: 'rooms/:roomNumber/edit',
         component: RoomEditComponent,
+      },
+      {
+        path: 'availability',
+        component: AvailabilityComponent,
       },
       { path: '', redirectTo: 'rooms', pathMatch: 'full' },
     ],
