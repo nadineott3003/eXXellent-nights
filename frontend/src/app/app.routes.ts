cb9b64be25e6513bba16a4c012cc01ac466a9dd6
Nadine Ott <info@nadineott.de>
2025-12-09 13:15:06 +0100

    [frontend] initial layout setup: add header, main layout and first component for rooms list

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Route } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { RoomListComponent } from './features/rooms/pages/room-list/room-list.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'rooms',
+        component: RoomListComponent,
+      },
+
+      { path: '', redirectTo: 'rooms', pathMatch: 'full' },
+    ],
+  },
+];
