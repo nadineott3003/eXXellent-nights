3872b7b07e5151af63f4dfaf4a64da9defd9c60e
Nadine Ott <info@nadineott.de>
2025-12-09 16:51:55 +0100

    [backend] add modal for room deletion

--- frontend/src/app/features/rooms/pages/room-list/room-list.component.ts
+++ frontend/src/app/features/rooms/pages/room-list/room-list.component.ts
@@ -2,22 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { RoomDto } from '../../../../core/api/generated/models/room-dto';
 import { RoomsService } from '../../../../core/api/generated/services/rooms.service';
+import { RoomDeleteModal } from '../../components/room-delete/room-delete-modal.component';
 
 @Component({
   standalone: true,
   selector: 'app-room-list',
-  imports: [RouterLink],
+  imports: [RouterLink, RoomDeleteModal],
   templateUrl: './room-list.component.html',
 })
 export class RoomListComponent implements OnInit {
   roomDtoArr: RoomDto[] = [];
+  deleteRoomNumber: string | null = null;
 
   constructor(private roomsService: RoomsService) {}
 
   ngOnInit() {
-    this.roomsService
-      .getRooms()
-      .then((rooms) => (this.roomDtoArr = rooms))
-      .catch((err) => console.error(err));
+    this.loadRooms();
+  }
+
+  async loadRooms() {
+    this.roomDtoArr = await this.roomsService.getRooms();
+  }
+
+  openDeleteModal(roomNumber: string) {
+    this.deleteRoomNumber = roomNumber;
+  }
+
+  closeDeleteModal() {
+    this.deleteRoomNumber = null;
+  }
+
+  async handleDeleted() {
+    this.deleteRoomNumber = null;
+    await this.loadRooms();
   }
 }
